85a8a4625e63a6f84bb60909c99924ed2ba4bac2
Yedam101 <109948144+Yedam101@users.noreply.github.com>
2023-01-22 21:49:24 +0900

    0122

--- airbnb-clone-backend/rooms/views.py
+++ airbnb-clone-backend/rooms/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError
-
+from django.db import transaction
 from categories.models import Category
 from .models import Amenity, Room
 from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
@@ -22,21 +22,41 @@ class Rooms(APIView):
             if serializer.is_valid():
                 category_pk = request.data.get("category") # 유저가 보낸 데이터로 카테고리의 id 찾음
                 if not category_pk: # 유저가 카테고리 안 보냈으면
-                    raise ParseError # 에러 반환
+                    raise ParseError("Category is required.") # 에러 반환 그냥 ParseError 라고만 써도 되지만 구체적 원인을 써줌.
                 try:
                     category = Category.objects.get(pk=category_pk) # 카테고리 보냈으면 DB에서 유저가 보낸 카테고리에 맞는 카테고리 꺼내옴
                     if category.kind == Category.CategoryKindChoices.EXPERIENCES: # 그런데 그 카테고리 id가 EXPERIENCES의 카테고리이면
-                        raise ParseError # 에러 반환
+                        raise ParseError("The category kind should be 'rooms'") # 에러 반환
                 except Category.DoesNotExist: # DB에 카테고리가 없다면
-                    raise ParseError # 에러 반환
+                    raise ParseError("Category not found") # 에러 반환
+                '''
                 new_room = serializer.save(owner=request.user, category=category)
+                amenities = request.data.get("amenities") # amenities는 ManyToMany 필드라 여러개가 들어올 수 있음.
+                for amenity_pk in amenities:
+                    try:
+                        amenity = Amenity.objects.get(pk=amenity_pk)
+                    except Amenity.DoesNotExist:
+                        new_room.delete()
+                        raise ParseError(f"Amenity with id {amenity_pk} not found")
+                    new_room.amenities.add(amenity) # .add()는 ManyToMany 필드일 때. 삭제는 room.amenities.remove로 가능. ForeignKey는 ex) room.owner = new_owner 처럼.
                 return Response(RoomDetailSerializer(new_room).data)
+                '''
+                try: # 중간에 실패하면 new_room.delete()하는 대신 transaction으로 과정 묶어줌
+                    with transaction.atomic():
+                        new_room = serializer.save(owner=request.user, category=category)
+                        amenities = request.data.get("amenities")
+                        for amenity_pk in amenities:
+                            amenity = Amenity.objects.get(pk=amenity_pk)
+                            new_room.amenities.add(amenity)
+                        return Response(RoomDetailSerializer(new_room).data)
+                except Exception:
+                    raise ParseError("Amenity not found")
             else: # serializer validation 통과 못 함
                 return Response(serializer.errors) # 에러 반환 
         else: # 로그인 안 된 유저
             raise NotAuthenticated
 
-
+  
 
 class RoomDetail(APIView):
 
